09076eaaedd49e1b97a748402337c365fdb0e006
Sukesh kappuram <iamsukeshk@hotmail.com>
2024-02-09 11:58:07 +0530

    Bug Fixes

--- src/environments/environment.blue.ts
+++ src/environments/environment.blue.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: true,
   isDemoVersion: true,
-  envName: 'blue',
-  coreApiUrl: 'http://uatcafm.rynasolutions.com/CoreAPI/api',
-  amApiUrl: 'http://uatcafm.rynasolutions.com/AssetMgmtApi/api',
-  ppmApiUrl: 'http://uatcafm.rynasolutions.com/PMApi/api',
-  stockApiUrl: 'http://uatcafm.rynasolutions.com/StockApi/api',
-  scheduledTaskApiUrl: 'http://uatcafm.rynasolutions.com/SchduledApi/api',
-  reportApiUrl: 'http://uatcafm.rynasolutions.com/ReportApi/api',
-  hrmsApiUrl: 'http://uatcafm.rynasolutions.com/HRMSMgmtApi/api',
-  quoteApiUrl: 'http://uatcafm.rynasolutions.com/QuoteMgmtApi/api',
+  envName: 'uat',
+  coreApiUrl: 'https://apicustomer.rynaifm.com/CustomerApi/api',
+  amApiUrl: 'https://apicustomer.rynaifm.com/AssetMgmtApi/api',
+  ppmApiUrl: 'https://apicustomer.rynaifm.com/CustomerApi/api',
+  stockApiUrl: 'https://apicustomer.rynaifm.com/StockApi/api',
+  scheduledTaskApiUrl: 'https://apicustomer.rynaifm.com/SchduledApi/api',
+  reportApiUrl: 'https://apicustomer.rynaifm.com/ReportApi/api',
+  hrmsApiUrl: 'https://apicustomer.rynaifm.com/HRMSMgmtApi/api',
+  quoteApiUrl: 'https://apicustomer.rynaifm.com/QuoteMgmtApi/api',
   ver: '2'
 };
 
